c8fbfc75671affebc87bc73745eacfc3b6f92efa
rakibul181 <rakibulhasan0641@gmail.com>
2022-10-23 13:58:57 +0600

    add server

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 const port = process.env.PORT || 5000
+app.use(cors())
 
-
-const category = require('./data/categories.json')
+const categories = require('./data/categories.json')
+const  news = require('./data/news.json')
 
 app.get('/',(req, res)=>{
     res.send('News APi running')
 });
 
-app.get('category',(req,res)=>{
-    res.send
+app.get('/categories',(req,res)=>{
+    res.send(categories)
+})
+app.get('/news',(req,res)=>{
+    res.send(news)
 })
 
 app.listen(port, ()=>{
