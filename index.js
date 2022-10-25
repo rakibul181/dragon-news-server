cdcd9f5b65de7f9097526e5927dcbb78fabe36f2
rakibul181 <rakibulhasan0641@gmail.com>
2022-10-25 15:42:11 +0600

    REady to deploy

--- index.js
+++ index.js
@@ -6,19 +6,37 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 const categories = require('./data/categories.json')
-const  news = require('./data/news.json')
+const news = require('./data/news.json')
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('News APi running')
 });
 
-app.get('/categories',(req,res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories)
 })
-app.get('/news',(req,res)=>{
+
+app.get('/news',(req, res)=>{
     res.send(news)
 })
 
-app.listen(port, ()=>{
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selected_news = news.find(n => n._id === id);
+    res.send(selected_news)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const filteredCategory = news.filter(n => n.category_id === id)
+        res.send(filteredCategory)
+    }
+    // console.log(filteredCategory);
+})
+app.listen(port, () => {
     console.log('Dragon News Server Running');
 })
